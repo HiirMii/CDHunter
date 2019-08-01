09ce594fdf99a59f501255f0e38ccb63094afd7b
HiirMii <hiirmii@gmail.com>
2019-08-01 08:49:49 +0100

    Change App Widget functionality

--- app/src/main/java/com/example/android/cdhunter/widget/WishlistWidget.java
+++ app/src/main/java/com/example/android/cdhunter/widget/WishlistWidget.java
@@ -10,25 +10,59 @@ import android.widget.RemoteViews;
 import androidx.annotation.NonNull;
 
 import com.example.android.cdhunter.R;
+import com.example.android.cdhunter.model.album.Album;
 import com.example.android.cdhunter.ui.main.MainActivity;
+import com.example.android.cdhunter.utils.Constants;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WishlistWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        assert user != null;
+        String userId = user.getUid();
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.wishlist_widget);
-
-        setRemoteAdapter(context,views);
-
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-        views.setOnClickPendingIntent(R.id.widget_wishlist_title, pendingIntent);
+        views.setOnClickPendingIntent(R.id.wishlist_count, pendingIntent);
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference()
+                .child(Constants.ALBUMS).child(userId);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Album album = new Album();
+                int wishlistCount = 0;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    album = snapshot.getValue(Album.class);
+                    assert album != null;
+                    if (album.getOwnershipStatus().equals(Constants.WISHLIST)) {
+                        wishlistCount +=1;
+                    }
+                }
+                views.setTextViewText(R.id.wishlist_count, String.valueOf(wishlistCount));
+                // Instruct the widget manager to update the widget
+                appWidgetManager.updateAppWidget(appWidgetId, views);
+                appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.wishlist_count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -47,12 +81,6 @@ public class WishlistWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
-    }
-
-    private static void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
-        views.setRemoteAdapter(R.id.widget_list_view, new Intent(context, WishlistWidgetService.class));
-        views.setEmptyView(R.id.widget_list_view, R.id.widget_empty_list_view);
-
     }
 }
 
